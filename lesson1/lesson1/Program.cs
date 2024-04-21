ee166d5fd7dd6730202c9e4f1c6454200f2e77ca
MigRaid <128039643+MigRaid@users.noreply.github.com>
2024-04-21 13:55:35 +0300

    Add files via upload

---
+++ lesson1/lesson1/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace lesson1 // Название
+{
+    internal class Program // Класс
+    {
+        static void Main(string[] args) // Метод
+        {
+            Console.WriteLine("Hello, World!");
+            Console.ReadKey();
+        }
+    }
+}
